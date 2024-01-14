eb415eb2b9060c499bf0e271eeb890a01451d2e2
= <=>
2024-01-14 17:17:55 +0100

    Finished common pipes on all pages components

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
+
+//Configuracion del locale de la App
+import localeEsEA from '@angular/common/locales/es-EA'
+import localeFrCA from '@angular/common/locales/fr-CA'
+import { registerLocaleData } from '@angular/common';
+
+//Estable el locale en angular
+registerLocaleData(localeEsEA);
+registerLocaleData(localeFrCA);
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+  ],
+  //Idioma por defecto
+  providers: [
+    {
+    provide: LOCALE_ID, useValue: 'es-EA'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-uncommon-page',
+  templateUrl: './uncommon-page.component.html',
+
+})
+export class UncommonPageComponent {
+
+
+
+
+
+// i18n Select
+public name: string = 'Fernando';
+public gender: 'male' | 'female' = 'male';
+
+public invitationMap = {
+  male: 'invitarlo',
+  female: 'invitarla'
+}
+
+changeClient(): void {
+ this.name = 'Melissa';
+ this.gender = 'female';
+}
+
+//i18Plural
+public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia']
+public clientsMap = {
+  '=0': 'no tenemos ningun cliente esperando',
+  '=1': 'tenemos 1  cliente esperando',
+  'other': ' tenemos # clientes esperando',
+
+}
+deleteClient() {
+    this.clients.pop();
+  }
+
+//KeyValuePipe
+public person = {
+  name:'Fernando',
+  age:36,
+  adress:'Otawwa, Canada'
+}
+
+//AsyncPipe
+
+public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap:', value) ),
+);
+
+public promiseValue:Promise<string> = new Promise((resolve, reject) => {
+  setTimeout( () => {
+    resolve('Tenemos datos en la promesa');
+    console.log('Tenemos datos en la promesa');
+  }, 3500)
+
+})
+
+}
